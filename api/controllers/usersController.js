4bab8ba0f5fe179a9f426e5028b1c0ac6b4960a9
Ewerton Nascimento <epnascimento.dev@gmail.com>
2022-08-29 12:01:59 -0300

    cadastro de usuário

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -1,4 +1,5 @@
 const database = require("../models");
+const bcrypt = require("bcrypt");
 
 class UsersController {
     static async listarUsuarios(req, res) {
@@ -22,35 +23,58 @@ class UsersController {
 
 	static async cadastrarUsuario(req, res){
 		const { usuario } = req.body
-		const novoUsuario = req.body
 		try{
 			const usuarioExiste = await database.Users.findOne({where: {usuario}});
 			if(usuarioExiste) {
 				return res.status(500).json({message: "Usuário já existe!"});
 			}
+
+			bcrypt.hash(req.body.senha, 12, async (errBcrypt, hash) =>{
+				if (errBcrypt) {
+					return res.status(500).json({ error: errBcrypt });
+				}
+				const criarNovoUsuario = await database.Users.create({usuario: req.body.usuario, email: req.body.email, senha: hash});
+				return res.status(201).json(criarNovoUsuario);
+			});
 			
-			const criarNovoUsuario = await database.Users.create(novoUsuario);
-			return res.status(201).json(criarNovoUsuario);
 		} catch (error){
-			return res.status(500).json(error.message);
+			return res.status(500).json({ message: 'Não foi possível cadastrar o usuário !'});
 		}
 	}
 
 	static async atualizarUsuario(req, res){
 		const { usuario } = req.body
 		const { id } = req.params
-		const novasInfos = req.body
+		let usuarioExiste;
 		try{
-			const usuarioExiste = await database.Users.findOne({where: {usuario}});
-			if(usuarioExiste) {
+			usuario ? usuarioExiste = await database.Users.findOne({where: {usuario}}) : undefined
+
+			if(usuarioExiste){
 				return res.status(500).json({message: "Usuário já existe!"});
 			}
 
-			await database.Users.update(novasInfos, {where: {id: Number(id)}});
+			if(req.body.usuario != null && req.body.usuario != undefined && req.body.usuario != ''){
+				await database.Users.update({ usuario: req.body.usuario }, {where: {id: Number(id)}});
+			}
+
+			if(req.body.email != null && req.body.email != undefined && req.body.email != ''){
+				await database.Users.update({ email: req.body.email }, {where: {id: Number(id)}});
+			}
+
+			if(req.body.senha != null && req.body.senha != undefined && req.body.senha != ''){
+				bcrypt.hash(req.body.senha, 12, async (errBcrypt, hash) =>{
+					if (errBcrypt) {
+						return res.status(500).json({ error: errBcrypt });
+					}
+	
+					await database.Users.update({senha: hash}, {where: {id: Number(id)}});
+				});
+			}
+			
 			const usuarioAtualizado = await database.Users.findOne({where: {id: Number(id)}});
 			return res.status(200).json(usuarioAtualizado);
 		} catch (error){
-			return res.status(500).json(error.message);
+			return res.status(500).json({ message: 'Não foi possível atualizar o usuário !'});
 		}
 	}
 
@@ -70,7 +94,7 @@ class UsersController {
 			await database.Users.restore({ where: {id: Number(id)}});
 			return res.status(200).json({message: `Usuário de id: ${id} foi restaurado com sucesso !`})
 		} catch (error) {
-			return res.status(500).json(error.message);
+			return res.status(500).json({ message: 'Não foi possível restaurar este usuário !'});
 		}
 	}
 }
